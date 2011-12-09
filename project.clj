710d9f51898fcaa4570924e01c6515ddb57e77b7
Kovas Boguta <kovasb@Kovass-MacBook-Air.local>
2011-12-09 15:19:30 -0800

    remove clojure 1.2.1 dependency

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject cljs-jquery "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :description "Easiest way to manipulate the DOM from clojurescript"
+  :dependencies [])
